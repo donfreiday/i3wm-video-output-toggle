992eb9042ca722c6b350c8d2b599ee25282b0c8b
donfreiday <donfreiday@gmail.com>
2018-02-12 22:27:35 -0500

    Change gamma to 0.8 to correct for too-bright defaults.

--- toggle.sh
+++ toggle.sh
@@ -20,11 +20,10 @@ message=""
 case $state in
   [0]*)
   message="LVDS1 auto"
-  xrandr --output LVDS1 --auto --output HDMI1 --off --output --VGA1 --off
+  xrandr --output LVDS1 --auto --gamma 0.8:0.8:0.8 --output HDMI1 --off --output --VGA1 --off
   ;;
   [1]*)
-  message="LVDS1 720 | HDMI1 1080"
-  xrandr --fb 1920x1080 --output LVDS1 --mode 1366x768 --scale-from 1920x1080 --output HDMI1 --mode 1920x1080 --scale 1x1 --same-as LVDS1
+  xrandr --fb 1920x1080 --output LVDS1 --mode 1366x768 --scale-from 1920x1080 --gamma 0.8:0.8:0.8 --output HDMI1 --mode 1920x1080 --scale 1x1 --same-as LVDS1
   ;;
   [2]*)
   message="HDMI1 auto"
@@ -32,7 +31,7 @@ case $state in
   ;;
   [3]*)
   message="LVDS1 auto | HDMI1 auto, right of LVDS1"
-  xrandr --output LVDS1 --auto --output HDMI1 --auto --right-of LVDS1
+  xrandr --output LVDS1 --auto --gamma 0.8:0.8:0.8 --output HDMI1 --auto --right-of LVDS1
   ;;
 esac
 
