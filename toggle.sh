21c79ff95bc0a81ec398f314031f38e2fe3f79ca
donfreiday <donfreiday@gmail.com>
2018-04-16 08:00:26 -0400

    Reduce built in display gamma.

--- toggle.sh
+++ toggle.sh
@@ -13,18 +13,18 @@ fi
 state=`expr ${state} + 1`
 if [[ state -ge output_states ]] ; then
   state=0
-fi
+fiq
 
 message=""
 
 case $state in
   [0]*)
   message="LVDS1 auto"
-  xrandr --output LVDS1 --auto --gamma 0.8:0.8:0.8 --output HDMI1 --off --output --VGA1 --off
+  xrandr --output LVDS1 --auto --gamma 0.7:0.7:0.7 --output HDMI1 --off --output --VGA1 --off
   ;;
   [1]*)
   message="HDMI1 auto, LVDS1 scaled"
-  xrandr --fb 1920x1080 --output LVDS1 --mode 1366x768 --scale-from 1920x1080 --gamma 0.8:0.8:0.8 --output HDMI1 --mode 1920x1080 --scale 1x1 --same-as LVDS1
+  xrandr --fb 1920x1080 --output LVDS1 --mode 1366x768 --scale-from 1920x1080 --gamma 0.7:0.7:0.7 --output HDMI1 --mode 1920x1080 --scale 1x1 --same-as LVDS1
   ;;
   [2]*)
   message="HDMI1 auto"
@@ -32,7 +32,7 @@ case $state in
   ;;
   [3]*)
   message="LVDS1 auto | HDMI1 auto, right of LVDS1"
-  xrandr --output LVDS1 --auto --gamma 0.8:0.8:0.8 --output HDMI1 --auto --right-of LVDS1
+  xrandr --output LVDS1 --auto --gamma 0.7:0.7:0.7 --output HDMI1 --auto --right-of LVDS1
   ;;
 esac
 
